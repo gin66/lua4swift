ff1aa57c4f15dbdfa57243f56240fdfc7843ea4f
Jochen Kiemes <jochen@kiemes.de>
2021-06-17 23:33:58 +0200

    restructure package

--- Package.swift
+++ Package.swift
@@ -1,15 +1,37 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "lua4swift",
+    name: "CLua",
     products: [
-        .library(name: "lua4swift", targets: ["lua4swift"]),
-        .library(name: "LuaSource", targets: ["LuaSource"]),
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Lua",
+            targets: ["Lua"]),
+        .library(
+            name: "CLua",
+            targets: ["CLua"]),
     ],
-    dependencies: [],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
-        .target(name: "lua4swift", dependencies: ["LuaSource"], path: "Lua"),
-        .target(name: "LuaSource", dependencies: [], path: "LuaSource")
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Lua",
+            dependencies: ["CLua"]),
+        .testTarget(
+            name: "LuaTests",
+            dependencies: ["Lua","CLua"]),
+        .target(
+            name: "CLua",
+            dependencies: []),
+//        .testTarget(
+//            name: "CLuaTests",
+//            dependencies: ["CLua"]),
     ]
 )
---
+++ Sources/Lua/Lua.h
@@ -0,0 +1,11 @@
+@import Foundation;
+@import CLua;
+
+//! Project version number for Lua.
+FOUNDATION_EXPORT double LuaVersionNumber;
+
+//! Project version string for Lua.
+FOUNDATION_EXPORT const unsigned char LuaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lua/PublicHeader.h>
+
